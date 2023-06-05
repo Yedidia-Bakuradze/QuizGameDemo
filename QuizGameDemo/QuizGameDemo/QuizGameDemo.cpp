df6d65bf310a7ce1a2e591dd6eacc98bec313703
Yedidia Bakuradze <yedidiadev@gmail.com>
2023-06-05 16:28:09 +0300

    Program crashs immediately - Bug fixed.

--- QuizGameDemo/QuizGameDemo/QuizGameDemo.cpp
+++ QuizGameDemo/QuizGameDemo/QuizGameDemo.cpp
@@ -11,6 +11,20 @@ struct player {
 	int socre;
 };
 
+/*Decalring functions : */
+
+//Shows the user "all" his personal information:
+void profileData(player user);
+
+//Main function of profile modification:
+void mainProfile(player user);
+
+//The modification function:
+void modifyProfile(player user);
+
+
+
+
 //Local database:
 player listOfPlayers[10] = {};
 enum dashBoradOptions {profile,runQuiz,createQuiz,logout};
@@ -36,17 +50,18 @@ int main() {
 		cout << row;
 		cout << "Hello " << currentPlayer.firstName << "" << endl;
 		cout << "Your current score: " << currentPlayer.socre << "." << endl;
-		cout << row;
 
-		//Player's options:
-		cout << "Press 0 to see and modify your personal information." << endl;
-		cout << "Press 1 to run a quiz." << endl;
-		cout << "Press 2 to creare a quiz." << endl;
-		cout << "Press 3 to logout." << endl;
-		cout << row;
 
 		//Main gateway:
 		do {
+			//Player's options:
+			cout << row;
+			cout << "Press 0 to see and modify your personal information." << endl;
+			cout << "Press 1 to run a quiz." << endl;
+			cout << "Press 2 to creare a quiz." << endl;
+			cout << "Press 3 to logout." << endl;
+			cout << row;
+			
 			cout << ">>>";
 			cin >> choice;
 
@@ -74,17 +89,6 @@ int main() {
 }
 
 
-//Shows the user "all" his personal information:
-void profileData(player user) {
-	//User's profile
-	cout << row;
-	cout << "This is " << user.firstName << "'s profile." << endl;
-	cout << "Full name: " << user.firstName << " " << user.lastName << '.' << endl;
-	cout << "Username: " << user.username << "." << endl;
-	cout << "Total score: " << user.socre << '.' << endl;
-	cout << row;
-}
-
 //Main function of profile modification:
 void mainProfile(player user) {
 	char choice;
@@ -94,7 +98,7 @@ void mainProfile(player user) {
 
 	//User's choice handler:
 	do {
-		cout << "Would you like to change and modify any of your personl information? (y/n)";
+		cout << "Would you like to change and modify any of your personl information? (y/n)\n>>>";
 		cin >> choice;
 		
 		switch (choice) {
@@ -117,6 +121,17 @@ void mainProfile(player user) {
 	cout << "Returning to the dashboard ... " << endl;
 }
 
+//Shows the user "all" his personal information:
+void profileData(player user) {
+	//User's profile
+	cout << row;
+	cout << "This is " << user.firstName << "'s profile." << endl;
+	cout << "Full name: " << user.firstName << " " << user.lastName << '.' << endl;
+	cout << "Username: " << user.username << "." << endl;
+	cout << "Total score: " << user.socre << '.' << endl;
+	cout << row;
+}
+
 
 //The modification function:
 void modifyProfile(player user) {
@@ -135,39 +150,39 @@ void modifyProfile(player user) {
 
 
 	//The gateway to the profile database:
-	do {
-		cout << ">>>";
-		cin >> choice;
-		switch (choice) {
-		case 0:
-			cout << "Enter your new first name: ";
-			cin >> user.firstName;
-			break;
-		case 1:
-			cout << "Enter your new last name: ";
-			cin >> user.lastName;
-			break;
-		case 2:
-			cout << "Enter your new password: ";
-			cin >> newPassword;
-			/* Check is the password is valid. */
-			user.password = newPassword;
-			break;
-		case 3:
-			cout << "Enter your new username: ";
-			cin >> newUsername;
-			/* Check is the username is valid. */
-			user.username = newUsername;
-			break;
+	cout << ">>>";
+	cin >> choice;
 
-		case 4:
-			cout << "Saving changes ... " << endl;
-			break;
+	switch (choice) {
+	case 0:
+		cout << "Enter your new first name: ";
+		cin >> user.firstName;
+		break;
+	case 1:
+		cout << "Enter your new last name: ";
+		cin >> user.lastName;
+		break;
+	case 2:
+		cout << "Enter your new password: ";
+		cin >> newPassword;
+		/* Check is the password is valid. */
+		user.password = newPassword;
+		break;
+	case 3:
+		cout << "Enter your new username: ";
+		cin >> newUsername;
+		/* Check is the username is valid. */
+		user.username = newUsername;
+		break;
 
-		default:
-			cout << "Please choose a number between 0 to 3." << endl;
-			break;
-		}
-	} while (choice != 4);
+	case 4:
+		cout << "Saving changes ... " << endl;
+		break;
+
+	default:
+		cout << "Please choose a number between 0 to 3." << endl;
+		break;
+	}
+
 
 }
